621b6f32455eabdf79e31b9cec6484e66dec2102
unknown <h1537@yuki-cn.yuki233.com>
2018-02-15 01:00:18 +0900

    命令输入

--- Aria_webui.sh
+++ Aria_webui.sh
@@ -36,3 +36,23 @@ s()(
 #        #mv $DLR/download/nohup.out "$log/$(date +"%Y_%m_%d_%H_%M_%S")Closed.log"
     }
 )
+
+while true; do
+    [ -z "$HELPED" ] && echo '查看帮助请输入 help'
+    HELPED=1
+    read -e -p '请选择希望执行的操作> ' CMD
+    [ "$CMD" = quit ] && quit
+    [ "$CMD" =    q ] && quit
+    ## 检测输入的命令有没有存在对应函数
+    test -z "$CMD" && continue ## 输入命令为空
+    type -t "$CMD" | grep -q function || {
+        echo "命令 $CMD 没找到，查看帮助请输入 help"
+        continue 
+    }
+    ## 测试模式下不实际执行命令，而是显示命令内容
+    if [ -n "$TEST" ] ; then
+        type "$CMD"
+    else
+        "$CMD"
+    fi
+done
