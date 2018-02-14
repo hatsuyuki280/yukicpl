bf89880cc46856dde9cf34232b43c76ab700b8dd
unknown <h1537@yuki-cn.yuki233.com>
2018-02-15 00:56:29 +0900

    233

---
+++ Aria2c_rpc_web_seting.sh
@@ -0,0 +1,14 @@
+#!/bin/bash 
+
+apt install -y aria2    ##安装Aria2
+
+
+
+
+mkdir -p /yuki/site/dl.sm.yuki233.com   ##创建站点目录
+
+
+
+sudo adduser dl ##创建一个名为dl的用户
+
+sudo chown -R dl  /yuki/site/dl.sm.yuki233.com/download/    ##授权用户dl拥有./download/的访问权
---
+++ Aria_webui.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+echo "  [雪次元AriaWebUI控制面板]
+     请选择希望执行的操作
+          [o] 启动
+          [s] 停止
+          [c] 设置
+          [q] 退出
+"
+test -e ~/.yukicpl/aria.temp && {
+    source ~/.yukicpl/aria.temp
+}
+test -e ~/.yukicpl/aria.conf || {
+    setup
+}
+source ~/.yukicpl/aria.conf
+
+c()(
+    echo 暂不可用
+
+    echo 请输入下载目录
+
+    source ~/.yukicpl/aria.conf
+)
+
+o()(
+    cd /yuki/site/dl.sm.yuki233.com/download/ 
+    sudo -u dl nohup aria2c --enable-rpc &
+#    cd $DLR/download/ 
+#    sudo -u dl nohup aria2c --enable-rpc &
+)
+
+s()(
+    pkill -f aria2c
+#    test "$logsl" = "Y" && {
+#        mv /yuki/site/dl.sm.yuki233.com/download/nohup.out "$log/$(date +"%Y_%m_%d_%H_%M_%S")Closed.log"
+#        #mv $DLR/download/nohup.out "$log/$(date +"%Y_%m_%d_%H_%M_%S")Closed.log"
+    }
+)
