8ebbbb206179428a82f585c841d2e57bc3eb64d3
hatsuyuki280 <yuki@mail.yuki233.com>
2018-02-19 14:36:36 +0900

    修改了自动下载的地址

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash 
 ## 雪次元服务器控制面板安装程序
-sudo wget -O /usr/local/bin/yukicpl 一个地址
+sudo wget -O /usr/local/bin/yukicpl https://raw.githubusercontent.com/hatsuyuki280/yukicpl/master/yukicpl.sh
 chmod +x /usr/local/bin/yukicpl
+echo 安装完成，下次使用直接输入 yukicpl 即可
