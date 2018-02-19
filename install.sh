058284f198175b61aeb7ffb37dc5cb8271a77dd4
hatsuyuki280 <yuki@mail.yuki233.com>
2018-02-19 13:53:25 +0900

    创建了安装和更新脚本

---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/bin/bash 
+## 初雪的服务器控制面板
+## 本文件可以放在 /usr/local/bin/yukicpl ，并 chmod +x /usr/local/bin/yukicpl 以方便执行
+## 更新 git 版可以在 bash 执行 sudo wget -O /usr/local/bin/yukicpl 一个地址 ; sudo chmod +x /usr/local/bin/yukicpl
