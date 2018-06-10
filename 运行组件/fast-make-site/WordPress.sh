20816fced6ba80723367efd4a428f044c1d48454
初雪 <yuki233dayo@gmail.com>
2018-06-11 00:03:54 +0900

    一键包的计划

--- 运行组件/fast-make-site/WordPress.sh
+++ 运行组件/fast-make-site/WordPress.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+FLAG=$1     ##传参标记
+DN=$2       ##绑定的域名
+DIR=$3      ##站点根目录
+SQLT=$4     ##数据库类型
+SQLN=$5     ##数据库名
+USER=$6     ##数据库用户
+PASS=$7     ##数据库密码
+
+
+
+echo " $FLAG" | grep -q ' -flag.on' && TEST=1
