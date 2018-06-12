fe521e9cba3bcd762c05eb2431ebd9256a6a3468
hatsuyuki280 <yuki@mail.yuki233.com>
2018-06-12 18:15:46 +0900

    总会用到的嘛

---
+++ 运行组件/onek/wordpress-yuki.sh
@@ -0,0 +1,16 @@
+
+
+
+
+
+echo 开始下载 Wordpress
+
+wget https://wordpress.org/latest.zip
+
+echo 正在解压
+
+echo 正在获取某dalao的主题包
+
+echo 内含可爱的live2d角色的那种
+
+wget 
---
+++ 运行组件/onek/wordpress.sh
@@ -0,0 +1,10 @@
+
+
+
+
+
+echo 开始下载 Wordpress
+
+wget https://wordpress.org/latest.zip
+
+echo 正在解压
