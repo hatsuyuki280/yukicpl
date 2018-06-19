b570ebc54a972b8aac87f7af3b8b9c1face62b91
hatsuyuki280 <yuki@mail.yuki233.com>
2018-06-20 00:35:02 +0900

    更新？

--- 运行组件/onek/wordpress.sh
+++ 运行组件/onek/wordpress.sh
@@ -8,3 +8,11 @@ echo 开始下载 Wordpress
 wget https://wordpress.org/latest.zip
 
 echo 正在解压
+
+apt update
+apt install -y unar
+
+unar latest.zip
+rm latest.zip
+mv ./wordpress/* .
+rm -rf ./wordpress
--- 运行组件/onekey.sh
+++ 运行组件/onekey.sh
@@ -1,17 +1,3 @@
-<<<<<<< HEAD
-#!/bin/bash
-FLAG=$1     ##传参标记
-DN=$2       ##绑定的域名
-DIR=$3      ##站点根目录
-SQLT=$4     ##数据库类型
-SQLN=$5     ##数据库名
-USER=$6     ##数据库用户
-PASS=$7     ##数据库密码
-
-
-
-echo " $FLAG" | grep -q ' -flag.on' && TEST=1
-=======
 #!/bin/bash
 FLAG=$1     ##传参标记
 SITEN=$2    ##一键包名
@@ -35,4 +21,3 @@ echo " $FLAG" | grep -q ' -flag.on' || {
 
 
 }
->>>>>>> c4f8a7a3332442c084635ed39e32e9d7fa41721c
