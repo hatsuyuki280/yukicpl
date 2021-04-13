89f724cd87eba8509a99f3fa83c88fbc8529cf7d
hatsuyuki280 <yuki233dayo@gmail.com>
2021-04-13 16:00:10 +0000

    creat ddns

---
+++ lib/cf-ddns.py
@@ -0,0 +1,9 @@
+#!/bin/env python3
+
+
+###
+# Set block
+###
+
+get_ip = [4,6]    # 4 for ipv4 6 for ipv6, if you choices 4 and 6, please make sure you have set a record and aaaa record both.
+ddns_domain = ["ipv4.example.com","ipv6.example.com"]  # 
