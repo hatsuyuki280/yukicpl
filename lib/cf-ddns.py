064a6985f762f949aff7745719224e4c82051e7d
hatsuyuki280 <yuki233dayo@gmail.com>
2021-04-14 10:01:19 +0000

    添加了用于获取ip地址的部分

--- lib/cf-ddns.py
+++ lib/cf-ddns.py
@@ -24,3 +24,23 @@ http_header = {'Content-Type': "application/json"}
 # API Token Test part
 ###
 
+
+
+
+###
+# Get IP
+###
+try:
+    respons = get(get_ip_from['v4']).json()
+    ipv4 = ipaddress.IPv4Address(respons[ip_index_name])
+    print("Success for get IPv4 in : " + ipv4.compressed)
+except:
+    ipv4 = ipaddress.IPv4Address('0.0.0.0')
+    print("Can't get IPv4, Will not change any thing")
+try:
+    respons = get(get_ip_from['v6']).json()
+    ipv6 = ipaddress.IPv6Address(respons[ip_index_name])
+    print("Success for get IPv6 in : " + ipv6.compressed)
+except:
+    ipv6 = ipaddress.IPv6Address('::1')
+    print("Can't get IPv6, Will not change any thing")
