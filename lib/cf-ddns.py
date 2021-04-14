56ab70a954ade5b91e7fc5a91d27d811a81039ff
hatsuyuki280 <yuki233dayo@gmail.com>
2021-04-14 09:30:09 +0000

    take some change

--- lib/cf-ddns.py
+++ lib/cf-ddns.py
@@ -1,9 +1,26 @@
 #!/bin/env python3
+from requests import get, post
+import ipaddress
 
 
 ###
 # Set block
 ###
+ddns_domain = {'v4':['ddns-ipv4.example.com'],'v6':['ddns-ipv6.example.com']}   # you can set only one domain but make sure you already added record from cloudflare by manualy.
+                                                                                # if you have more than one domain need to set ddns record, please set with array formart. 
+cloudflare_auth = { 'X-Auth-Email': "",
+                    'X-Auth-Key': ""}
 
-get_ip = [4,6]    # 4 for ipv4 6 for ipv6, if you choices 4 and 6, please make sure you have set a record and aaaa record both.
-ddns_domain = ["ipv4.example.com","ipv6.example.com"]  # 
+###
+# change this part only for you know what you do 
+###
+get_ip_from = {'v4': "https://api-ipv4.ip.sb/jsonip", 'v6': "https://api-ipv6.ip.sb/jsonip"}     # please set api full-uri to get json.
+ip_index_name = ["ip"]    # please set index in this var, if have more than 1 level index, pleas set all use array formart.
+cloudflare_api_base_url = "https://api.cloudflare.com/client/v4/"
+http_header = {'Content-Type': "application/json"}
+
+
+###
+# API Token Test part
+###
+
