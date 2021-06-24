239bbe0c8ad127e7f1466636dfaa911aaca5c503
初雪 <yuki233dayo@gmail.com>
2021-06-24 17:28:41 +0900

    更新了一点有趣的东西 在某地看到了有趣的视频，这个是里面用html画铃仙的代码，有人把代码开源了，于是咱拉了过来quq

--- GitPage/dist/dev/lib/Shigureader.sh
+++ GitPage/dist/dev/lib/Shigureader.sh
@@ -1,7 +1,16 @@
 pre_install(){
+    ## 提升权限
     sudo -i
+    ## nodejs 的安装脚本 
+    curl -fsSL https://deb.nodesource.com/setup_16.x | bash -
+    ## yarn 的安装脚本
+    curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | \
+    sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
+    echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | \
+    sudo tee /etc/apt/sources.list.d/yarn.list
+    ## 通过 apt 更新并按转所需软件包
     apt update && apt -y full-upgrade
-    apt install -y nodejs npm p7zip imagemagick git mocha unar wget nginx
+    apt install -y nodejs yarn npm p7zip imagemagick git mocha unar wget nginx gcc g++ make
 
     mkdir -p /yukicpl/opt
     git clone https://github.com/hjyssg/ShiguReader
@@ -10,7 +19,7 @@ pre_install(){
 
 npm_install(){
     sudo -i
-    npm install --prefix .
+    yarn install
 
     
 }
