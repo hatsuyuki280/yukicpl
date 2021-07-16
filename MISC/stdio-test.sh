3d99a565b465e824e2af0816e4a021b44b8d68cf
初雪 <yuki233dayo@gmail.com>
2021-07-16 18:06:26 +0900

    stdio - test

--- MISC/stdio-test.sh
+++ MISC/stdio-test.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
 echo " $@"
+echo $@ | grep -q "--full" && easy=0
+echo $@ | grep -q "--ja" && lang="ja"
+echo $@ | grep -q "--en" && lang="en"
+
+echo $lang
+
+[ $easy -eq 0 ] && echo "will full install"
